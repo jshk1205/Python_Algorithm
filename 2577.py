4c6911b785dcc34a92e286c994cb5b346f0e3494
dlwlstjd1205 <dlwlstjd1205@gmail.com>
2022-04-11 20:06:16 +0900

    백준 2577 숫자의 개수

--- 2577.py
+++ 2577.py
@@ -3,7 +3,6 @@ B = int(input())
 C = int(input())
 temp = str(A * B * C)
 count = [0 for i in range(10)]
-temp_list = list(map(int,temp))
 for i in temp_list:
     count[i] += 1
 for k in range(len(count)):
