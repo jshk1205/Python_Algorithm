68107c48c309b6afbb5572af9aa23a3cf13ac216
dlwlstjd1205 <dlwlstjd1205@gmail.com>
2022-04-12 17:59:59 +0900

    백준 1065 한수

---
+++ 1065.py
@@ -0,0 +1,9 @@
+num = int(input())
+count = 0
+for i in range (1, num+1):
+    num_list = list(map(int, str(i)))
+    if i < 100:
+        count += 1
+    elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
+        count +=1
+print(count)
