9d98b53f71a5cb3a050e81ae84d7ab65832ff16a
dlwlstjd1205 <dlwlstjd1205@gmail.com>
2022-04-15 21:55:49 +0900

    백준 2292 벌집

--- 2292.py
+++ 2292.py
@@ -1,5 +1,5 @@
 n = int(input())
-round, num, size = 1, 1, 6 #round -> 지나가야 하는 방의 수, size -> 커지는 크기
+round, num, size = 1, 1, 6 #round -> 지나가야 하는 방의 수,num -> 비교할 수, size -> 커지는 크기
 while num < n:
     round += 1
     num += size
