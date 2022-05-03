512c0511822e10b9bb672d1fb561808b0f96091f
jinseong <dlwlstjd1205@gmail.com>
2022-05-03 11:21:15 +0900

    백준 11279 최대 힙

---
+++ 11279.py
@@ -0,0 +1,14 @@
+import sys
+import heapq as hpq
+n = int(sys.stdin.readline())
+num_list =[]
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if len(num_list) == 0:
+            print(0)
+        else:
+            print(hpq.heappop(num_list)[1])
+
+    elif x > 0:
+        hpq.heappush(num_list, (-x,x))
